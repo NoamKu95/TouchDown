ee4145b73f23ac1923db0ed5f6ecbbf3f94f9756
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-11 11:38:08 +0200

    Added FooterView for Homepage

---
+++ TouchDown/TouchDown/Views/Home/FooterView.swift
@@ -0,0 +1,41 @@
+//
+//  FooterView.swift
+//  TouchDown
+//
+//  Created by Noam Kurtzer on 11/03/2023.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack (alignment: .center, spacing: 10) {
+            Text("We offer the most cuttig edge, comfortable, lightweight and durable football helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright - Noam Kurtzer\nAll rights reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        }
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+            .padding()
+    }
+}
