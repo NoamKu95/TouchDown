adf36b136aa87b7c1d513db07fdb6cf90dc474a0
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-11 11:17:51 +0200

    Added folders and Colors constants file

---
+++ TouchDown/TouchDown/Utils/Constants/Colors.swift
@@ -0,0 +1,11 @@
+//
+//  Colors.swift
+//  TouchDown
+//
+//  Created by Noam Kurtzer on 11/03/2023.
+//
+
+import SwiftUI
+
+let colorBackground = Color("colorBackground")
+let colorGray = Color(UIColor.systemGray4)
