7459b788c209a095fab0e442df8158b72b31625a
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-11 12:16:45 +0200

    Embedded the NavigationBarView in the Homepage

--- TouchDown/TouchDown/Screens/ContentView.swift
+++ TouchDown/TouchDown/Screens/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack (spacing: 0) {
+                NavigationBarView()
+                    .padding()
+                    .background(.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y:5)
+                    
+                Spacer()
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
