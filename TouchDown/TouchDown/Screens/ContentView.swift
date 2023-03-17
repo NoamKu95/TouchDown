2b82a8978e99a785abde3e22e7d659b0cc94d7bf
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-17 20:22:42 +0200

    Created the Shop - ObservableObject and implemented in the ContentView

--- TouchDown/TouchDown/Screens/ContentView.swift
+++ TouchDown/TouchDown/Screens/ContentView.swift
@@ -9,39 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @EnvironmentObject var shop: Shop
+    
     var body: some View {
         ZStack {
-            VStack (spacing: 0) {
-                NavigationBarView()
-                    .padding()
-                    .background(.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y:5)
-                
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    VStack (spacing: 0) {
-                        HeroCarouselView()
-                            .padding(.vertical, 20)
-                            .frame(minHeight: 150, idealHeight: 250, maxHeight: .infinity, alignment: .center)
-                        
-                        CategoryGridView()
-                        
-                        TitleView(title: "Helmets")
-                        LazyVGrid (columns: gridLayout, spacing: 15) {
-                            ForEach (productsList) { product in
-                                ProductItemView(product: product)
+            if shop.selectedProduct == nil {
+                VStack (spacing: 0) {
+                    NavigationBarView()
+                        .padding()
+                        .background(.white)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y:5)
+                    
+                    ScrollView(.vertical, showsIndicators: false, content: {
+                        VStack (spacing: 0) {
+                            HeroCarouselView()
+                                .padding(.vertical, 20)
+                                .frame(minHeight: 150, idealHeight: 250, maxHeight: .infinity, alignment: .center)
+                            
+                            CategoryGridView()
+                            
+                            TitleView(title: "Helmets")
+                            LazyVGrid (columns: gridLayout, spacing: 15) {
+                                ForEach (productsList) { product in
+                                    ProductItemView(product: product)
+                                        .onTapGesture {
+                                            shop.selectedProduct = product
+                                        }
+                                }
                             }
+                            .padding(15)
+                            
+                            TitleView(title: "Brands")
+                            BrandsGridView()
+                            
+                            FooterView()
+                                .padding(.horizontal)
                         }
-                        .padding(15)
-                        
-                        TitleView(title: "Brands")
-                        BrandsGridView()
-                        
-                        FooterView()
-                            .padding(.horizontal)
-                    }
-                })
+                    })
+                }
+                .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+            } else {
+                ProductDetails()
             }
-            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }
     }
 }
@@ -50,5 +59,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewDevice("iPhone 12 Pro")
+            .environmentObject(Shop())
     }
 }
---
+++ TouchDown/TouchDown/Utils/ShopClass.swift
@@ -0,0 +1,12 @@
+//
+//  ShopClass.swift
+//  TouchDown
+//
+//  Created by Noam Kurtzer on 17/03/2023.
+//
+
+import Foundation
+
+class Shop: ObservableObject {
+    @Published var selectedProduct: Product? = nil
+}
